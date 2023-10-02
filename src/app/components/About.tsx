0da4fb9be13ed7ff6117879c223fba8cc93b3c6c
Bart <bartoszswierzynski7@gmail.com>
2023-10-02 05:10:55 -0500

    progress made on carousel

---
+++ src/app/components/About.tsx
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h2 className="text-xl font-bold text-center my-2">About Me</h2>
+      <p>
+        I&apos;m a <span className="text-orange-500">Front-end Developer</span> with
+        <span className="text-orange-500">full stack capabilities</span> currently working at a
+        <span className="text-orange-500">public health non-profit organization</span>. I&apos;m a recent
+        <span className="text-orange-500"> DePaul University graduate</span> with a{" "}
+        <span className="text-orange-500">Bachelor&apos;s Degree</span> in
+        <span className="text-orange-500"> Computer Science</span> and a <span className="text-orange-500">curious developer</span> who
+        genuinely loves to learn <span className="text-orange-500">cutting-edge technologies</span> that elevate my skills as a
+        developer.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import DropdownButton from "./components/dropdown";
 import Navbar_ from "./components/Navbar";
 import Hero from "./components/Hero";
 import Carousel from "./components/Carousel";
+import About from "./components/About";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
       {/* TODO - navbar */}
       {/* <Navbar_ /> */}
       <Hero />
+      <About />
       {/* TODO - test flowbite component */}
       {/* <DropdownButton /> */}
       <Carousel />
