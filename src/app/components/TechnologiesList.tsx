14492335c67fbc693c5e66a8e2c250f00a2c57cf
bartswierz <bartoszswierzynski7@gmail.com>
2023-10-10 17:47:46 -0500

    fixed technology list container layout

--- src/app/components/TechnologiesList.tsx
+++ src/app/components/TechnologiesList.tsx
@@ -3,16 +3,14 @@ import Image from "next/image";
 // Displays the list of technologies that we currently know and/or familiar with
 const TechnologiesList = () => {
   return (
-    <ul className="grid grid-cols-9 auto-cols-min  text-white justify-center my-8 max-w-[800px]">
+    <ul className="flex flex-row flex-wrap  text-white justify-center my-8 max-w-[720px]">
       {technologyIconsList.map(({ name, iconPath }) => {
         return (
-          <li key={name} className="self-center- b w-[80px]">
-            {/* <div className="b"> */}
+          <li key={name} className="self-center- b w-[80px] shrink-0">
             <div className="max-w-[40px] max-h-[40px] xl:max-w-[60px] xl:max-h-[60px] w-full self-center mx-auto">
-              <Image src={iconPath} className="w-full- h-full- b-" alt={`${name} Icon`} width={60} height={60} />
+              <Image src={iconPath} className="" alt={`${name} Icon`} width={60} height={60} />
             </div>
             <p className="text-center text-[10px] font-semibold">{name}</p>
-            {/* </div> */}
           </li>
         );
       })}
