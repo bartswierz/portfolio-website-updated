ddd70ac7fccaa9c6f5f8518bf40aaafb6da62d0a
bartswierz <bartoszswierzynski7@gmail.com>
2023-10-10 05:58:45 -0500

    minor design changes to contact form

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -4,14 +4,18 @@ import { Label, TextInput, Textarea } from "flowbite-react";
 import { HiMail } from "react-icons/hi";
 import { BsFillPersonFill } from "react-icons/bs";
 const ContactForm = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    console.log("user submitted form");
+  };
   return (
-    <div className="border-[3px] border-white max-w-[550px] w-full bg-primary p-4 rounded-lg my-8">
+    <form onSubmit={handleSubmit} className="border-[3px] border-white max-w-[550px] w-full bg-primary p-4 rounded-lg my-8 ">
       <h2 className="text-white text-center text-2xl font-semibold">Contact Me</h2>
       <div className="flex flex-col justify-center items-center gap-2">
         {/* NAME */}
         <div className="max-w-md w-full">
           <div className="mb-2 block">
-            <Label htmlFor="username3" value="Name*" className="font-semibold" />
+            <Label htmlFor="username3" value="Name*" className="font-semibold text-white" />
           </div>
           <TextInput icon={BsFillPersonFill} id="username3" placeholder="Michael Scott" required />
         </div>
@@ -19,7 +23,7 @@ const ContactForm = () => {
         {/* EMAIL */}
         <div className="max-w-md w-full">
           <div className="mb-2 block">
-            <Label htmlFor="email4" value="Email*" className="font-semibold" />
+            <Label htmlFor="email4" value="Email*" className="font-semibold text-white" />
           </div>
           <TextInput icon={HiMail} id="email4" placeholder="michaelscott@gmail.com" required type="email" />
         </div>
@@ -27,7 +31,7 @@ const ContactForm = () => {
         {/* TEXT AREA */}
         <div className="max-w-md w-full my-2" id="textarea">
           <div className="mb-2 block">
-            <Label htmlFor="comment" value="Leave a message (optional)" className="font-semibold" />
+            <Label htmlFor="comment" value="Leave a message (optional)" className="font-semibold text-white" />
           </div>
           <Textarea id="comment" placeholder="Leave a comment..." required rows={4} />
         </div>
@@ -36,12 +40,12 @@ const ContactForm = () => {
       <div className="flex justify-center items-center mt-4">
         <button
           type="submit"
-          className="text-primary dark:text-dark w-[80%] py-2 bg-transparent rounded-full border-2 border-primary font-semibold hover:bg-hover hover:text-hover"
+          className="text-dark w-[80%] py-2 bg-transparent rounded-full border-2 border-primary font-semibold hover:bg-hover hover:text-hover"
         >
           Submit
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
