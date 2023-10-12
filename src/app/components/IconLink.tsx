fee8e6b6f1903ff4b663f00b5db0d4d05a65b475
bartswierz <bartoszswierzynski7@gmail.com>
2023-10-11 22:47:06 -0500

    fixed typescript error for IconLink component loading component before values are ready

--- src/app/components/IconLink.tsx
+++ src/app/components/IconLink.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 import { AiFillFilePdf } from "react-icons/ai";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 import { FiMail } from "react-icons/fi";
-import { CgUnavailable } from "react-icons/cg";
+
 interface IconLinkProps {
   linkType: "github" | "linkedin" | "resume" | "contact";
 }
 
 const IconLink = ({ linkType = "github" }: IconLinkProps) => {
-  let icon: JSX.Element;
-  let text: string;
-  let path: string;
+  let icon: JSX.Element | null = null;
+  let text: string | null = null;
+  let path: string | null = null;
 
   switch (linkType) {
     case "github":
@@ -33,34 +33,39 @@ const IconLink = ({ linkType = "github" }: IconLinkProps) => {
       text = "Contact";
       path = "#contact";
       break;
-    default: //setting default to contact form
+    default:
       break;
   }
 
-  if (linkType !== "contact" && linkType !== "resume" && linkType !== "linkedin" && linkType !== "github") {
+  // While we are waiting for icon, text, and path to be defined, render nothing
+  if (!icon || !text || !path) {
     return null;
   }
 
-  // IF CONTACT, USER LINK TO SCROLL DOWN TO OUR CONTACT FORM
-  if (linkType === "contact")
-    return (
-      <Link href={path}>
-        <div className="flex flex-col items-center">
-          {icon}
-          <span className="text-sm">{text}</span>
-        </div>
-      </Link>
-    );
-
-  // NOT A CONTACT FORM, USE LINK TO OPEN LINK IN A NEW TAB
-  return (
-    <Link href={path} target="_blank">
-      <div className="flex flex-col items-center">
-        {icon}
-        <span className="text-sm">{text}</span>
-      </div>
-    </Link>
-  );
+  // Once we have icon, text, and path, render the link
+  if (icon && text && path) {
+    // IF CONTACT, USE LINK TO SCROLL DOWN TO OUR CONTACT FORM
+    if (linkType === "contact") {
+      return (
+        <Link href={path}>
+          <div className="flex flex-col items-center">
+            {icon}
+            <span className="text-sm">{text}</span>
+          </div>
+        </Link>
+      );
+    } else {
+      // NOT A CONTACT FORM, USE LINK TO OPEN LINK IN A NEW TAB
+      return (
+        <Link href={path} target="_blank">
+          <div className="flex flex-col items-center">
+            {icon}
+            <span className="text-sm">{text}</span>
+          </div>
+        </Link>
+      );
+    }
+  }
 };
 
 export default IconLink;
