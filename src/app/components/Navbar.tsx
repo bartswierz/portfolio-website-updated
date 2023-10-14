1ff61563d7669ef27914e60336a159e247caa59e
Bart <bartoszswierzynski7@gmail.com>
2023-10-14 04:22:15 -0500

    updated navbar links active state

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,13 @@
 "use client";
-import Link from "next/link";
 import { DarkThemeToggle, Flowbite, Navbar } from "flowbite-react";
+import { useState } from "react";
 
 const Navbar_ = () => {
+  const [active, setActive] = useState("home");
+
+  // on hover: underline will expand from the the left to the right
+  const underlineStyle = "block max-w-0 group-hover:max-w-full transition-all duration-300 h-[3px] bg-brand";
+
   return (
     // <Navbar fluid rounded className="fixed z-[9999] w-full">
     <Navbar fluid rounded className="fixed z-[999] w-full">
@@ -19,25 +24,34 @@ const Navbar_ = () => {
       </div>
       {/* NAV LINKS */}
       <Navbar.Collapse>
-        <Navbar.Link active href="#work" className="group text-gray-300 transition duration-300">
-          <p>Work Projects</p>
-          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5- h-[3px] bg-brand"></span>
+        <Navbar.Link
+          href="#"
+          // className={`${active === "home" ? "text-dark" : ""} group text-gray-300 transition duration-300`}
+          className={`group text-gray-300 transition duration-300`}
+          onClick={() => setActive("home")}
+        >
+          <p className={`${active === "home" ? "text-gray-200" : ""} `}>Home</p>
+          <span className={`${underlineStyle} ${active === "home" ? "max-w-full" : ""}`}></span>
         </Navbar.Link>
-        <Navbar.Link href="#personal" className="group text-gray-300 transition duration-300">
-          <p>Personal Projects</p>
-          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5- h-[3px] bg-brand"></span>
+        <Navbar.Link href="#work" className="group text-gray-300 transition duration-300" onClick={() => setActive("work")}>
+          <p className={`${active === "work" ? "text-gray-200" : ""} `}>Work Projects</p>
+          <span className={`${underlineStyle} ${active === "work" ? "max-w-full" : ""}`}></span>
         </Navbar.Link>
-        <Navbar.Link href="#about" className="group text-gray-300 transition duration-300">
-          <p>About</p>
-          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5- h-[3px] bg-brand"></span>
+        <Navbar.Link href="#personal" className="group text-gray-300 transition duration-300" onClick={() => setActive("personal")}>
+          <p className={`${active === "personal" ? "text-gray-200" : ""} `}>Personal Projects</p>
+          <span className={`${underlineStyle} ${active === "personal" ? "max-w-full" : ""}`}></span>
         </Navbar.Link>
-        <Navbar.Link href="#contact" className="group text-gray-300 transition duration-300">
-          <p>Contact</p>
-          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5- h-[3px] bg-brand"></span>
+        <Navbar.Link href="#about" className="group text-gray-300 transition duration-300" onClick={() => setActive("about")}>
+          <p className={`${active === "about" ? "text-gray-200" : ""} `}>About</p>
+          <span className={`${underlineStyle} ${active === "about" ? "max-w-full" : ""}`}></span>
+        </Navbar.Link>
+        <Navbar.Link href="#contact" className="group text-gray-300 transition duration-300" onClick={() => setActive("contact")}>
+          <p className={`${active === "contact" ? "text-gray-200" : ""} `}>Contact</p>
+          <span className={`${underlineStyle} ${active === "contact" ? "max-w-full" : ""}`}></span>
         </Navbar.Link>
         <Navbar.Link href="./files/Bartosz_Swierzynski.pdf" className="group text-gray-300 transition duration-300" target="_blank">
           <p>Resume</p>
-          <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5- h-[3px] bg-brand"></span>
+          <span className={underlineStyle}></span>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
