a65666fad5187b47e71011d0ab3d84413771f7c0
Bart <bartoszswierzynski7@gmail.com>
2023-10-16 02:11:33 -0500

    updated footer layout

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,11 +3,11 @@ import IconLink from "./IconLink";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary- bg-black/90- bg-black- text-white w-screen- pt-2 relative h-screen- b w-full bg-green-500- b- min-h-max">
+    <footer className="bg-primary  text-white w-full- w-screen min-h-max h-max py-4 ">
       {/* Background */}
       {/* <div className="absolute- top-0- left-0- right-0- bottom-0- border-green-500 w-screen h-screen-"> */}
       {/* <div className="absolute- top-0 left-0 w-full h-full"> */}
-      <svg
+      {/* <svg
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -16,20 +16,14 @@ const Footer = () => {
         width="100%"
         height="100%"
         viewBox="0 0 1600 900"
-        className="absolute- top-0 left-0 right-0- bottom-0- w-screen- h-screen h-max- scale-[400%]- max-h-[500px]- b"
+        className="absolute- top-0 left-0 right-0 bottom-0 w-screen- h-screen h-max- scale-[200%]- max-h-[500px] min-h-full- b"
       >
         <defs>
           <linearGradient id="bg" x2="0%" y2="100%">
-            {/* <stop offset="0%" stopColor="rgba(53, 127, 242, 0.6)"></stop> */}
-            <stop offset="0%" stopColor="rgba(53, 127, 242, 0.5)"></stop>
-            {/* <stop offset="100%" stopColor="rgba(38, 89, 190, 0.06)"></stop> */}
-            <stop offset="100%" stopColor="rgba(38, 89, 190, 0.5)"></stop>
+            <stop offset="0%" stopColor="rgba(53, 127, 242, 0.6)"></stop>
+
+            <stop offset="100%" stopColor="rgba(38, 89, 190, 0.06)"></stop>
           </linearGradient>
-          {/* ORIGINAL */}
-          {/* <linearGradient id="bg" x2="0%" y2="100%">
-              <stop offset="0%" style="stop-color: rgba(53, 127, 242, 0.6)"></stop>
-              <stop offset="100%" style="stop-color: rgba(38, 89, 190, 0.06)"></stop>
-            </linearGradient> */}
           <path
             id="wave"
             fill="url(#bg)"
@@ -37,7 +31,7 @@ const Footer = () => {
       s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"
           />
         </defs>
-        <g className="w-screen">
+        <g>
           <use xlinkHref="#wave" opacity=".3">
             <animateTransform
               attributeName="transform"
@@ -78,38 +72,36 @@ const Footer = () => {
             />
           </use>
         </g>
-      </svg>
-      {/* </div> */}
-      {/* </div> */}
+      </svg> */}
       {/* END OF BACKGROUND WAVES */}
 
-      {/* <div className="bg-blue-500- absolute bottom-0 left-0 flex flex-col justify-center items-center w-full"> */}
       <ul className="flex flex-row gap-2 w-full justify-center items-center text-dark">
         <IconLink linkType="github" animationDelay={"500ms"} />
         <IconLink linkType="linkedin" animationDelay={"500ms"} />
         <IconLink linkType="resume" animationDelay={"500ms"} />
       </ul>
-      <ul className="flex gap-2 justify-center py-2 ">
-        <li>
+      <ul className="flex gap-2 justify-center py-2 list-none md:list-disc list-inside flex-wrap mx-2 flex-col md:flex-row text-center">
+        <li className="pr-2">
           <Link href="#">Home</Link>
         </li>
-        <li>
+        {/* <span>&#x2022;</span> */}
+        <li className="pr-2">
           <Link href="#work">Work Projects</Link>
-          <Link href="#work" />
         </li>
-        <li>
+        {/* <span>&#x2022;</span> */}
+        <li className="pr-2">
           <Link href="#personal">Personal Projects</Link>
         </li>
-        <li>
+        {/* <span>&#x2022;</span> */}
+        <li className="pr-2">
           <Link href="#about">About</Link>
-          <Link href="#About" />
         </li>
-        <li>
+        {/* <span>&#x2022;</span> */}
+        <li className="pr-2">
           <Link href="#contact">Contact</Link>
         </li>
       </ul>
       <p className="text-center">BartoszSwierzynski.com © 2023 </p>
-      {/* </div> */}
     </footer>
   );
 };
