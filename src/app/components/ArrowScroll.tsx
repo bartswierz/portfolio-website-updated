a6621aecd6737eaa2f99b87ae2c5336d3e080f60
bartswierz <bartoszswierzynski7@gmail.com>
2023-10-18 22:57:33 -0500

    replaced nextjs link to basic a tag in arrow scroll to fix live site

--- src/app/components/ArrowScroll.tsx
+++ src/app/components/ArrowScroll.tsx
@@ -4,12 +4,12 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 const DropdownArrow = () => {
   return (
     <div className="absolute w-full bottom-10 left-[50vw]- left-0 flex justify-center items-center">
-      <Link
+      <a
         href="#about"
         className="w-[50px] h-[50px] rounded-full bg-gray-800 border-2 border-[#ffb700] hover:b flex justify-center items-center animate-bounce hover:border-secondary transition-all duration-300 scroll-smooth"
       >
         <RiArrowDropDownLine className="text-[#0072fe] w-[64px] h-[64px] hover:text-[#ffb700] transition-all duration-300" />
-      </Link>
+      </a>
     </div>
   );
 };
