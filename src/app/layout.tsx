18d63a61cc4808d00317020d3c320cce1061ec0d
bartswierz <bartoszswierzynski7@gmail.com>
2023-11-02 01:39:22 -0500

    updated background image to be a nextjs optimized image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import Navbar_ from "./components/Navbar/Navbar";
 import { Poppins } from "next/font/google";
 import Footer from "./components/Footer/Footer";
+import Image from "next/image";
+import AppBackground from "./components/AppBackground";
 
 export const metadata: Metadata = {
   title: "Bartosz Swierzynski - Portfolio",
@@ -20,11 +22,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="dark text-primary overflow-x-hidden">
       <body className={`${poppins.className} bg-gray-200 dark:from-slate-800- dark:bg-slate-900`}>
+        {/* BACKGROUND IMAGE - FIXED */}
+        <AppBackground />
         <Navbar_ />
         {children}
-        {/* <div className="absolute bottom-0 left-0 right-0 z-[-1]"> */}
         <Footer />
-        {/* </div> */}
       </body>
     </html>
   );
