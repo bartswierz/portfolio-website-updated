7218e971debe2d6582da0220830183530955a6a3
Bart <bartoszswierzynski7@gmail.com>
2023-11-05 21:51:08 -0600

    removed animationDelay prop from IconLink in footer to fix build error

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
   return (
     <footer className="bg-primary  text-white w-full- w-screen min-h-max h-max py-4 opacity-[75%]">
       <ul className="flex flex-row gap-2 w-full justify-center items-center text-dark">
-        <IconLink linkType="github" animationDelay={"500ms"} />
-        <IconLink linkType="linkedin" animationDelay={"500ms"} />
-        <IconLink linkType="resume" animationDelay={"500ms"} />
+        <IconLink linkType="github" />
+        <IconLink linkType="linkedin" />
+        <IconLink linkType="resume" />
       </ul>
       <ul className="flex gap-2 justify-center py-2 list-none md:list-disc list-inside flex-wrap mx-2 flex-col md:flex-row text-center transition-all">
         <li className="pr-2 first:list-none">
