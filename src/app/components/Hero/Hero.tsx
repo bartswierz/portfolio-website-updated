020c28dafaa7901f18ed3979c91f5ef4a555d7dc
Bart <bartoszswierzynski7@gmail.com>
2023-11-05 21:11:23 -0600

    updated hero component animations

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -4,33 +4,35 @@ import DropdownArrow from "./ArrowScroll";
 // Reference: https://flowbite.com/docs/components/jumbotron/
 const Hero = () => {
   return (
-    <section
-      className="w-full min-h-screen flex items-center justify-center relative"
-      // style={{
-      //   backgroundImage: "url('/images/mountainCodeBackground.jpg')",
-      //   backgroundSize: "cover",
-      //   backgroundPosition: "center",
-      //   backgroundRepeat: "no-repeat",
-      // }}
-    >
+    <section className="w-full min-h-screen flex items-center justify-center relative">
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 className="mb-4 text-4xl tracking-tight leading-none md:text-5xl lg:text-6xl text-white/90 animate-jump-in animate-once animate-duration-[800ms] animate-delay-500">
+        {/* HEADER */}
+        <h1 className="mb-4 text-4xl tracking-tight leading-none md:text-5xl lg:text-6xl text-white/90 animate-jump-in animate-once animate-duration-[800ms] animate-delay-[600ms] animate-ease-in-out">
           <span className="font-bold">Hi, I&apos;m</span> <span className="text-highlight font-semibold">Bart</span> |{" "}
           <span className="text-highlight font-semibold">Front-end Developer</span>
         </h1>
-        <h1 className="mb-4 text-xl font-normal text-gray-500 md:text-3xl sm:px-16 lg:px-48 dark:text-white/90 animate-jump-in animate-once animate-duration-[800ms] animate-delay-[600ms]">
+        {/* SUBHEADING #1 */}
+        <h1 className="mb-4 text-xl font-normal text-gray-500 md:text-3xl sm:px-16 lg:px-48 dark:text-white/90 animate-jump-in animate-once animate-duration-[800ms] animate-delay-[700ms] animate-ease-in-out">
           Turning <span className="text-highlight">ideas</span> into <span className="text-highlight">reality</span> by{" "}
           <span className="text-highlight">building</span>
         </h1>
-        <h1 className="mb-4 text-xl font-normal text-gray-500 md:text-2xl sm:px-16 lg:px-48 dark:text-white/90 animate-jump-in animate-once animate-duration-[800ms] animate-delay-[700ms]">
+
+        {/* SUBHEADING #2 */}
+        <h1 className="mb-4 text-xl font-normal text-gray-500 md:text-2xl sm:px-16 lg:px-48 dark:text-white/90 animate-jump-in animate-once animate-duration-[800ms] animate-delay-[800ms] animate-ease-in-out">
           Let&apos;s build something
         </h1>
 
         {/* LINKS */}
         <ul className="text-dark flex flex-row justify-center items-center gap-2 mb-2">
-          <IconLink linkType="linkedin" animationDelay={"1000ms"} />
-          <IconLink linkType="github" animationDelay={"3000ms"} />
-          <IconLink linkType="contact" animationDelay={"5000ms"} />
+          <div className={`animate-fade-down animate-once animate-duration-500 animate-delay-[900ms]`}>
+            <IconLink linkType="linkedin" />
+          </div>
+          <div className={`animate-fade-down animate-once animate-duration-500 animate-delay-[1000ms]`}>
+            <IconLink linkType="github" />
+          </div>
+          <div className={`animate-fade-down animate-once animate-duration-500 animate-delay-[1100ms]`}>
+            <IconLink linkType="contact" />
+          </div>
         </ul>
 
         <DropdownArrow />
--- src/app/components/IconLink.tsx
+++ src/app/components/IconLink.tsx
@@ -5,10 +5,10 @@ import { FiMail } from "react-icons/fi";
 
 interface IconLinkProps {
   linkType: "github" | "linkedin" | "resume" | "contact";
-  animationDelay?: string;
 }
 
-const IconLink = ({ linkType = "github", animationDelay = "2000ms" }: IconLinkProps) => {
+// const IconLink = ({ linkType = "github", animationDelay = "2000ms" }: IconLinkProps) => {
+const IconLink = ({ linkType = "github" }: IconLinkProps) => {
   let icon: JSX.Element | null = null;
   let text: string | null = null;
   let path: string | null = null;
@@ -48,7 +48,7 @@ const IconLink = ({ linkType = "github", animationDelay = "2000ms" }: IconLinkPr
     // IF CONTACT, USE LINK TO SCROLL DOWN TO OUR CONTACT FORM
     if (linkType === "contact") {
       return (
-        <Link href={path} className={`animate-fade-down animate-once animate-duration-500 animate-delay-[${animationDelay}]`}>
+        <Link href={path}>
           <div className="flex flex-col items-center group">
             <span className="group-hover:text-[#0072fe] transition-all duration-300">{icon}</span>
             <span className="text-sm group-hover:text-[#0072fe] transition-all duration-300">{text}</span>
@@ -58,11 +58,7 @@ const IconLink = ({ linkType = "github", animationDelay = "2000ms" }: IconLinkPr
     } else {
       // NOT A CONTACT FORM, USE LINK TO OPEN LINK IN A NEW TAB
       return (
-        <Link
-          href={path}
-          target="_blank"
-          className={`animate-fade-down animate-once animate-duration-500 animate-delay-[2001ms]- animate-delay-[${animationDelay}]`}
-        >
+        <Link href={path} target="_blank">
           <div className="flex flex-col items-center group">
             <span className="group-hover:text-[#0072fe] transition-all duration-300">{icon}</span>
             <span className="text-sm group-hover:text-[#0072fe] transition-all duration-300">{text}</span>
