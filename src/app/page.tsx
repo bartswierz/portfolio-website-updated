c7f825d2834587ac87a30b625f0d0b6315c9d9d3
bartswierz <bartoszswierzynski7@gmail.com>
2023-11-06 23:45:11 -0600

    updated toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,12 @@ import ContactForm from "./components/Contact/ContactForm";
 import Footer from "./components/Footer/Footer";
 import Project from "./components/Project/Project";
 import ProjectHeading from "./components/Project/ProjectHeading";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function Home() {
   // console.log("work project list: ", WorkProjectList);
   const workProject = WorkProjectList[0];
-
+  const success = true;
   // console.log("Work Project", workProject);
   return (
     // <main className="flex min-h-screen flex-col items-center p-4">
@@ -44,6 +45,40 @@ export default function Home() {
 
       {/* <div className="w-full relative"> */}
       {/* <Footer /> */}
+      {/* </div> */}
+      {/* <button
+        className="bg-blue-500 py-2 px-4"
+        onClick={() =>
+          toast.success("🦄 Wow so easy!", {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          })
+        }
+      >
+        Send
+      </button> */}
+      {/* <div className="w-min- b flex flex-row relative w-max"> */}
+      {/* {success && ( */}
+      {/* <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      /> */}
+      {/* <ToastContainer /> */}
+      {/* )} */}
       {/* </div> */}
     </main>
   );
