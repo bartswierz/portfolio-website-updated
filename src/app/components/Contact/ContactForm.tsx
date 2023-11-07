08d87a476727035aeffcecce938c864bfd51f020
bartswierz <bartoszswierzynski7@gmail.com>
2023-11-06 23:48:44 -0600

    updated our contact form and toast file using our updated code from navbar-update branch

--- src/app/components/Contact/ContactForm.tsx
+++ src/app/components/Contact/ContactForm.tsx
@@ -1,17 +1,21 @@
 "use client";
-import { useState, useRef, FormEvent, ChangeEvent } from "react";
+import { useState, useRef, FormEvent, ChangeEvent, use } from "react";
 import { Label, TextInput, Textarea } from "flowbite-react";
 import { HiMail } from "react-icons/hi";
 import { BsFillPersonFill } from "react-icons/bs";
-import Toast_ from "../Toast";
+import { Toast } from "../Toast";
 import hasIntersected from "../../utils/hasIntersected";
+import { toast } from "react-toastify";
+// import { useRouter } from "next/navigation";
 
 //REFERENCE VIDEO FOR nodemailer setup: https://www.youtube.com/watch?v=6DAozN-qxr0
 const ContactForm = () => {
+  // const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [textareaFocus, setTextareaFocus] = useState(false);
   const [nameFocus, setNameFocus] = useState(false);
   const [emailFocus, setEmailFocus] = useState(false);
+  const [isSubmitSuccessful, setIsSubmitSuccessful] = useState(false);
 
   // For contact form display/remove
   const contactFormIntersectionRef = useRef<HTMLDivElement>(null);
@@ -70,16 +74,27 @@ const ContactForm = () => {
     };
 
     console.log("data: ", data);
-    const response = await fetch("/api/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+    // const response = await fetch("/api/contact", {
+    const response = await toast.promise(
+      fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }),
+      // Three possible states of the promise, these will update from pending to success(or error)
+      {
+        pending: "Sending message...",
+        success: "Message sent successfully!",
+        error: "Error sending message",
       },
-      body: JSON.stringify(data),
-    });
+      // Styling for toast
+      { theme: "dark", position: "bottom-right" }
+    );
 
+    // SUCCESSFUL RESPONSE, RESETS THE FORM
     if (response.ok) {
-      console.log("Message sent successfully");
       setLoading(false);
 
       //RESETS THE FORM
@@ -88,16 +103,18 @@ const ContactForm = () => {
       setMessage("");
       setEmailInputValidity("");
       setNameInputValidity("");
-      // <Toast_ />;
+
+      // ROUTE USER BACK TO THE TOP OF THE APP
+      // const router = useRouter();
+      // router.push("#");
     }
 
+    // FAILED RESPONSE
     if (!response.ok) {
       const error = await response.json();
 
       if (error.message) {
-        // TODO - add a toast
-        // Toast(error message)
-        // <Toast_ />;
+        console.log("error message: ", error.message);
       }
       console.log("Error sending message");
       setLoading(false);
@@ -105,142 +122,145 @@ const ContactForm = () => {
   };
 
   return (
-    <div
-      ref={contactFormIntersectionRef}
-      className={`border-[3px] border-white max-w-[550px] w-full bg-primary p-4 rounded-lg my-8 mx-4
+    <>
+      <div
+        ref={contactFormIntersectionRef}
+        className={`border-[3px] border-white max-w-[550px] w-full bg-primary p-4 rounded-lg my-8 mx-4 
     ${
       contactFormHasIntersected
         ? "animate-fade-right animate-once animate-duration-[300ms] animate-ease-in transition-opacity opacity-100 duration-[500ms]"
         : "opacity-0"
     }`}
-    >
-      <form onSubmit={handleSubmit}>
-        <h2 className="text-white text-center text-2xl font-semibold">Contact Me</h2>
-        <div className="flex flex-col justify-center items-center">
-          {/* NAME INPUT */}
-          <div className="max-w-md w-full min-h-[107px]">
-            <div className="mb-2 block">
-              <Label htmlFor="username3" value="Name (required)" className="font-semibold text-white" />
+      >
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-white text-center text-2xl font-semibold">Contact Me</h2>
+          <div className="flex flex-col justify-center items-center">
+            {/* NAME INPUT */}
+            <div className="max-w-md w-full min-h-[107px]">
+              <div className="mb-2 block">
+                <Label htmlFor="username3" value="Name (required)" className="font-semibold text-white" />
+              </div>
+              <TextInput
+                icon={BsFillPersonFill}
+                id="username3"
+                placeholder="Michael Scott"
+                color={nameInputValidity} // 'failure' | 'success' | ''
+                onChange={(e) => handleNameChange(e)}
+                onFocus={() => setNameFocus(true)}
+                onBlur={() => setNameFocus(false)}
+                value={name}
+                helperText={
+                  //displays text IF input is invalid
+                  nameInputValidity === "failure" && (
+                    <p>
+                      <span className="font-medium">Must be at least 2 characters & no special characters</span>
+                    </p>
+                  )
+                }
+                style={{
+                  borderRadius: "0.375rem",
+                  borderColor: "rgba(209, 213, 219, 1)",
+                  // margin: "2px none",
+                  borderWidth: "2px",
+                  transition: "background-color 200ms ease-in-out",
+                  backgroundColor: name.length > 0 || nameFocus ? "rgba(244, 244, 244, 1)" : "rgba(209, 213, 219, 1)",
+
+                  // Apply focus styles conditionally
+                  ...(name.length > 0 || nameFocus
+                    ? {
+                        backgroundColor: "rgba(244, 244, 244, 1)", //active color
+                        borderColor: `#252839`, // Match Container Background Color
+                        // boxShadow: "0 0 0 2px #ffb700", // Ring Color
+                        boxShadow: "0 0 0 2px #fff", // Ring Color
+                      }
+                    : {}),
+                }}
+                required
+              />
             </div>
-            <TextInput
-              icon={BsFillPersonFill}
-              id="username3"
-              placeholder="Michael Scott"
-              color={nameInputValidity} // 'failure' | 'success' | ''
-              onChange={(e) => handleNameChange(e)}
-              onFocus={() => setNameFocus(true)}
-              onBlur={() => setNameFocus(false)}
-              value={name}
-              helperText={
-                //displays text IF input is invalid
-                nameInputValidity === "failure" && (
-                  <p>
-                    <span className="font-medium">Must be at least 2 characters & no special characters</span>
-                  </p>
-                )
-              }
-              style={{
-                borderRadius: "0.375rem",
-                borderColor: "rgba(209, 213, 219, 1)",
-                // margin: "2px none",
-                borderWidth: "2px",
-                transition: "background-color 200ms ease-in-out",
-                backgroundColor: name.length > 0 || nameFocus ? "rgba(244, 244, 244, 1)" : "rgba(209, 213, 219, 1)",
 
-                // Apply focus styles conditionally
-                ...(name.length > 0 || nameFocus
-                  ? {
-                      backgroundColor: "rgba(244, 244, 244, 1)", //active color
-                      borderColor: `#252839`, // Match Container Background Color
-                      // boxShadow: "0 0 0 2px #ffb700", // Ring Color
-                      boxShadow: "0 0 0 2px #fff", // Ring Color
-                    }
-                  : {}),
-              }}
-              required
-            />
-          </div>
+            {/* EMAIL INPUT  */}
+            <div className="max-w-md w-full min-h-[107px]">
+              <div className="mb-2 block">
+                <Label htmlFor="email4" value="Email (required)" className="font-semibold text-white" />
+              </div>
+              <TextInput
+                icon={HiMail}
+                id="email4"
+                color={emailInputValidity} // 'failure' | 'success' | ''
+                helperText={
+                  //displays text IF input is invalid
+                  emailInputValidity === "failure" && (
+                    <p>
+                      <span className="font-medium">Invalid Email - ex. name@gmail.com</span>
+                    </p>
+                  )
+                }
+                value={email}
+                placeholder="michaelscott@gmail.com"
+                // type="email"
+                type="email"
+                onChange={(e) => handleEmailChange(e)}
+                onFocus={() => setEmailFocus(true)}
+                onBlur={() => setEmailFocus(false)}
+                style={{
+                  borderRadius: "0.375rem",
+                  borderColor: "rgba(209, 213, 219, 1)",
+                  borderWidth: "2px",
+                  transition: "background-color 200ms ease-in-out",
+                  backgroundColor: email.length > 0 || emailFocus ? "rgba(244, 244, 244, 1)" : "rgba(209, 213, 219, 1)",
 
-          {/* EMAIL INPUT  */}
-          <div className="max-w-md w-full min-h-[107px]">
-            <div className="mb-2 block">
-              <Label htmlFor="email4" value="Email (required)" className="font-semibold text-white" />
+                  // Apply focus styles conditionally
+                  ...(email.length > 0 || emailFocus
+                    ? {
+                        backgroundColor: "rgba(244, 244, 244, 1)", //active color
+                        borderColor: `#252839`, // Match Container Background Color
+                        // boxShadow: "0 0 0 2px #ffb700", // Ring Color
+                        boxShadow: "0 0 0 2px #fff", // Ring Color
+                      }
+                    : {}),
+                }}
+                required
+              />
             </div>
-            <TextInput
-              icon={HiMail}
-              id="email4"
-              color={emailInputValidity} // 'failure' | 'success' | ''
-              helperText={
-                //displays text IF input is invalid
-                emailInputValidity === "failure" && (
-                  <p>
-                    <span className="font-medium">Invalid Email - ex. name@gmail.com</span>
-                  </p>
-                )
-              }
-              value={email}
-              placeholder="michaelscott@gmail.com"
-              // type="email"
-              type="email"
-              onChange={(e) => handleEmailChange(e)}
-              onFocus={() => setEmailFocus(true)}
-              onBlur={() => setEmailFocus(false)}
-              style={{
-                borderRadius: "0.375rem",
-                borderColor: "rgba(209, 213, 219, 1)",
-                borderWidth: "2px",
-                transition: "background-color 200ms ease-in-out",
-                backgroundColor: email.length > 0 || emailFocus ? "rgba(244, 244, 244, 1)" : "rgba(209, 213, 219, 1)",
 
-                // Apply focus styles conditionally
-                ...(email.length > 0 || emailFocus
-                  ? {
-                      backgroundColor: "rgba(244, 244, 244, 1)", //active color
-                      borderColor: `#252839`, // Match Container Background Color
-                      // boxShadow: "0 0 0 2px #ffb700", // Ring Color
-                      boxShadow: "0 0 0 2px #fff", // Ring Color
-                    }
-                  : {}),
-              }}
-              required
-            />
-          </div>
-
-          {/* TEXT AREA INPUT  */}
-          <div className="max-w-md w-full mb-2" id="textarea">
-            <div className="mb-2 block">
-              <Label htmlFor="comment" value="Leave a message (optional)" className="font-semibold text-white" />
-            </div>
-            <Textarea
-              id="comment"
-              placeholder="Leave a comment..."
-              value={message}
-              rows={4}
-              color={""}
-              onChange={(e) => setMessage(e.target.value)}
-              onFocus={() => setTextareaFocus(true)}
-              onBlur={() => setTextareaFocus(false)}
-              className={`min-h-[70px] rounded-lg border-gray-300 border-[2px] transition-colors duration-200 ease-in-out
+            {/* TEXT AREA INPUT  */}
+            <div className="max-w-md w-full mb-2" id="textarea">
+              <div className="mb-2 block">
+                <Label htmlFor="comment" value="Leave a message (optional)" className="font-semibold text-white" />
+              </div>
+              <Textarea
+                id="comment"
+                placeholder="Leave a comment..."
+                value={message}
+                rows={4}
+                color={""}
+                onChange={(e) => setMessage(e.target.value)}
+                onFocus={() => setTextareaFocus(true)}
+                onBlur={() => setTextareaFocus(false)}
+                className={`min-h-[70px] rounded-lg border-gray-300 border-[2px] transition-colors duration-200 ease-in-out
               ${message.length > 0 || textareaFocus ? "bg-gray-50 focus:ring-2 focus:ring-white focus:border-focus" : "bg-gray-300"}
               `}
-              required
-            />
+              />
+            </div>
           </div>
-        </div>
 
-        {/* SUBMIT BUTTON */}
-        <div className="flex justify-center items-center mt-4 mb-3">
-          <button
-            type="submit"
-            // When its loading, we will make the button gray and disable it
-            disabled={loading}
-            className="text-dark w-[80%] py-2 bg-transparent rounded-full border-2 border-primary font-semibold hover:bg-hover hover:text-hover transition-all duration-300 ease-in-out disabled:bg-gray-400 disabled:text-gray-100"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+          {/* SUBMIT BUTTON */}
+          <div className="flex justify-center items-center mt-4 mb-3">
+            <button
+              type="submit"
+              // When its loading, we will make the button gray and disable it
+              // disabled={loading}
+              className="text-dark w-[80%] py-2 bg-transparent rounded-full border-2 border-primary font-semibold hover:bg-hover hover:text-hover transition-all duration-300 ease-in-out disabled:bg-gray-400- disabled:text-gray-100-"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      {/* Displays toast when user submits the completed contact form */}
+      <Toast />
+    </>
   );
 };
 
